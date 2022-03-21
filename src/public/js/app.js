6c49b027b2114bca80ee49820efd6df7343d0209
Minji <zz1qwop@gmail.com>
2022-03-21 17:44:38 +0900

    Camera Switch

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,95 +1,71 @@
 const socket = io();
-// io : 자동적으로 back-end socket.io와 연결해주는 function
-// 알아서 socket.io를 실행하고 있는 서버를 찾음
 
-const welcome = document.getElementById("welcome");
-const form = welcome.querySelector("form");
-const room = document.getElementById("room");
+const myFace = document.getElementById("myFace");
+const muteBtn = document.getElementById("mute");
+const cameraBtn = document.getElementById("camera");
+const camerasSelect = document.getElementById("cameras");
 
-room.hidden = true;
+// stream = video + audio
 
-let roomName;
+let myStream;
+let muted = false;
+let cameraOff = false;
 
-function addMessage(message) {
-  const ul = room.querySelector("ul");
-  const li = document.createElement("li");
-  li.innerText = message;
-  ul.appendChild(li);
-}
-
-function handleMessageSubmit(event) {
-  event.preventDefault();
-  const input = room.querySelector("#msg input");
-  const value = input.value;
-  socket.emit("new_message", input.value, roomName, () => {
-    addMessage(`You : ${value}`);
-  });
-  input.value = "";
-}
-
-function handleNicknameSubmit(event) {
-  event.preventDefault();
-  const input = room.querySelector("#name input");
-  socket.emit("nickname", input.value);
-}
-
-function showRoom() {
-  welcome.hidden = true;
-  room.hidden = false;
-  const h3 = room.querySelector("h3");
-  h3.innerText = `Room ${roomName}`;
-  const msgForm = room.querySelector("#msg");
-  const nameForm = room.querySelector("#name");
-  msgForm.addEventListener("submit", handleMessageSubmit);
-  nameForm.addEventListener("submit", handleNicknameSubmit);
-}
-
-function handleRoomSubmit(event) {
-  event.preventDefault();
-  const input = form.querySelector("input");
-  socket.emit("enter_room", input.value, showRoom);
-  // callback : 서버로부터 실행되는 function
-  // 특정한 이벤트를 emit 할 수 있다. emit 하면 argument 보낼 수 있음.
-  roomName = input.value;
-  input.value = "";
-}
-
-form.addEventListener("submit", handleRoomSubmit);
-
-/* 
-  1. socketIO를 이용하면 모든 것이 message일 필요가 없다.
-  2. 어떠한 event든 emit 가능
-  3. 전송할 때 원하는 아무거나 전송 가능, 개수도 여러 개 가능
-  4. function : 끝날 때 실행되는 함수를 보내고 싶으면 마지막 인수에 넣는다.
-  */
-
-socket.on("welcome", (user, newCount) => {
-  const h3 = room.querySelector("h3");
-  h3.innerText = `Room ${roomName} (${newCount})`;
-  addMessage(`${user} arrived!`);
-});
-
-socket.on("bye", (left, newCount) => {
-  const h3 = room.querySelector("h3");
-  h3.innerText = `Room ${roomName} (${newCount})`;
-  addMessage(`${left} left!`);
-});
-
-socket.on("new_message", addMessage);
-
-socket.on("room_change", (rooms) => {
-  // 한 번 실행할 때 화면이 방의 목록을 그려준다.
-  // 다시 한 번 실행할 때 (room) 목룍이 비어있으면 아무것도 하지 않는다.
-  // 하지만 이미 그려둔 것은 유지되는 상황. -> if문으로 확인
-
-  const roomList = welcome.querySelector("ul");
-  roomList.innerHTML = "";
-  if (rooms.length === 0) {
-    return;
+async function getCameras() {
+  try {
+    const devices = await navigator.mediaDevices.enumerateDevices();
+    const cameras = devices.filter((device) => device.kind === "videoinput");
+    cameras.forEach((camera) => {
+      const option = document.createElement("option");
+      option.value = camera.deviceId;
+      option.innerText = camera.label;
+      camerasSelect.appendChild(option);
+    });
+  } catch (e) {
+    console.log;
   }
-  rooms.forEach((room) => {
-    const li = document.createElement("li");
-    li.innerText = room;
-    roomList.append(li);
-  });
-});
+}
+
+async function getMedia() {
+  try {
+    myStream = await navigator.mediaDevices.getUserMedia({
+      audio: true,
+      video: true,
+    });
+    // getUserMedia(cons : 우리가 무엇을 얻고 싶은지)
+    myFace.srcObject = myStream;
+    await getCameras();
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+getMedia();
+
+function handleMuteClick() {
+  myStream
+    .getAudioTracks()
+    .forEach((track) => (track.enabled = !track.enabled));
+  if (!muted) {
+    muteBtn.innerText = "Unmute";
+    muted = true;
+  } else {
+    muteBtn.innerText = "Mute";
+    muted = false;
+  }
+}
+function handleCameraClick() {
+  myStream
+    .getVideoTracks()
+    .forEach((track) => (track.enabled = !track.enabled));
+  if (cameraOff) {
+    cameraBtn.innerText = "Turn Camera Off";
+    cameraOff = false;
+  } else {
+    cameraBtn.innerText = "Turn Camera On";
+    cameraOff = true;
+  }
+}
+
+muteBtn.addEventListener("click", handleMuteClick);
+cameraBtn.addEventListener("click", handleCameraClick);
--- src/server.js
+++ src/server.js
@@ -1,8 +1,6 @@
 import http from "http";
 // import WebSocket from "ws";
-// import SocketIO from "socket.io";
-const { Server } = require("socket.io");
-const { instrument } = require("@socket.io/admin-ui");
+import SocketIO from "socket.io";
 import express from "express";
 
 const app = express();
@@ -13,107 +11,10 @@ app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => res.render("home"));
-// app.get("/*", (req, res) => res.redirect("/"));
+app.get("/*", (req, res) => res.redirect("/"));
 
 const httpServer = http.createServer(app); // http server
-//const wsServer = SocketIO(httpServer);
-// socket.io 설치 -> /socket.io/socket.io.js 라는 url을 준다.
-const wsServer = new Server(httpServer, {
-  cors: {
-    origin: ["https://admin.socket.io"],
-    credentials: true,
-  },
-});
-
-instrument(wsServer, {
-  auth: false,
-});
-
-function publicRooms() {
-  const {
-    sockets: {
-      adapter: { sids, rooms },
-    },
-  } = wsServer;
-  const publicRooms = [];
-  rooms.forEach((_, key) => {
-    if (sids.get(key) === undefined) {
-      publicRooms.push(key);
-    }
-  });
-  return publicRooms;
-}
-
-function countRoom(roomName) {
-  return wsServer.sockets.adapter.rooms.get(roomName)?.size;
-}
-
-wsServer.on("connection", (socket) => {
-  socket["nickname"] = "Anon";
-  socket.onAny((event) => {
-    console.log(`Socket Event : ${event}`);
-  });
-  socket.on("enter_room", (roomName, done) => {
-    socket.join(roomName);
-    done(); // back-end에서 호출하지만 front-end에서 실행된다.
-    socket.to(roomName).emit("welcome", socket.nickname, countRoom(roomName));
-    wsServer.sockets.emit("room_change", publicRooms()); // 모든 socket에게
-  });
-  socket.on("disconnecting", () => {
-    // 떠나기 직전
-    socket.rooms.forEach((room) =>
-      socket.to(room).emit("bye", socket.nickname, countRoom(room) - 1)
-    );
-  });
-  socket.on("disconnect", () => {
-    wsServer.sockets.emit("room_change", publicRooms());
-  });
-  socket.on("new_message", (msg, room, done) => {
-    socket.to(room).emit("new_message", `${socket.nickname} : ${msg}`);
-    done();
-  });
-  socket.on("nickname", (nickname) => (socket["nickname"] = nickname));
-});
-
-// 같은 서버에서 http, webSocket 둘 다 작동 시킴
-// 현재 http server가 필요한 이유 : views, static files, home, redirection이 필요하기 때문.
-
-// function handleConnection(socket) {
-//   console.log(socket); // 연결된 브라우저
-// }
-
-// 누가 서버에 연결하면 그 connection을 넣기 위한 용도
-/* 
-const wss = new WebSocket.Server({ server });
-
-const sockets = [];
-
-wss.on("connection", (socket) => {
-  sockets.push(socket);
-  socket["nickname"] = "Anon";
-  console.log("Connected to Browser ✅");
-  socket.on("close", () => console.log("disconnected from the Browser ❌"));
-  socket.on("message", (msg) => {
-    const message = JSON.parse(msg);
-    switch (message.type) {
-      case "new_message":
-        sockets.forEach((aSocket) =>
-          aSocket.send(`${socket.nickname} : ${message.payload}`)
-        );
-      case "nickname":
-        socket["nickname"] = message.payload;
-      // socket은 기본적으로 객체이다. socket 안에 정보 저장 가능!
-    }
-  }); // 각 브라우저를 위한 것이라 socket에 연결. 특정 socket에서 메세지를 받았을 때
-  // socket.send() : 서버가 아니라 socket의 메소드!
-});
-// func은 socket을 받음 : socket은 연결된 어떤 사람. 연결된 브라우저와의 contact 라인.
- */
+const wsServer = SocketIO(httpServer);
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 httpServer.listen(3000, handleListen);
-
-// 서로 다른 브라우저는 서로 메시지를 주고받지 못함
-// ex. 크롬과 서버 사이에 websocket이 있고
-// firefox와 서버 사이에 websocket이 있다.
-// firefox와 크롬이 대화를 하게 하려면 -> 누가 연결되었는지 알아야 함.
