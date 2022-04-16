81703af2e7a9f4f6498e0d413abf9645b4acccb5
Minji <zz1qwop@gmail.com>
2022-04-16 14:43:13 +0900

    Senders

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -88,6 +88,13 @@ function handleCameraClick() {
 
 async function handleCameraChange() {
   await getMedia(camerasSelect.value);
+  if (myPeerConnection) {
+    const videoTrack = myStream.getVideoTracks()[0];
+    const videoSender = myPeerConnection
+      .getSenders()
+      .find((sender) => sender.track.kind === "video");
+    videoSender.replaceTrack(videoTrack);
+  }
 }
 
 muteBtn.addEventListener("click", handleMuteClick);
